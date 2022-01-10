d249ec3932df8a1b70a1065a5c442c28218bd6ee
Martin <mselvenis@gmail.com>
2022-01-10 08:55:02 +0000

    argos class

--- backend/src/parsers/argos.ts
+++ backend/src/parsers/argos.ts
@@ -1,90 +1,121 @@
 import { Page } from "puppeteer";
 import { Page as StorePage } from "@prisma/client";
-import { Report } from "../main";
+import { Report, ReportError } from "../main";
 import parse, { HTMLElement } from "node-html-parser";
 import moment from "moment";
 
-export async function argos(
-  storePage: StorePage,
-  reports: Report[],
-  page: Page
-) {
-  let currentPageNumber: number = storePage.pageStartsAt;
-  let nextPageAvailable: boolean = true;
-  let nextPageUrl: string = storePage.url;
+export class Argos {
+  storePage: StorePage;
+  reports: Report[] = [];
+  page: Page;
 
-  while (nextPageAvailable) {
-    const report: Report = {
+  currentPageNumber: number;
+  nextPageAvailable: boolean;
+  nextPageUrl: string;
+  currentReport: Report;
+
+  constructor(storePage: StorePage, page: Page) {
+    this.storePage = storePage;
+    this.page = page;
+    this.currentPageNumber = storePage.pageStartsAt;
+    this.nextPageAvailable = true;
+    this.nextPageUrl = storePage.url;
+    this.currentReport = {
       startedAt: moment().toDate(),
-      page: nextPageUrl,
-      pageNumber: currentPageNumber,
+      page: this.nextPageUrl,
+      pageNumber: this.currentPageNumber,
       errors: [],
       nextPageAvailable: false,
     };
+  }
 
-    await page.goto(nextPageUrl, {
+  onError(error: ReportError) {
+    this.currentReport.errors.push(error);
+  }
+
+  onReportFinish() {
+    this.currentReport.finishedAt = moment().toDate();
+    console.log(this.currentReport);
+  }
+
+  resetReport() {
+    this.currentReport = {
+      startedAt: moment().toDate(),
+      page: this.nextPageUrl,
+      pageNumber: this.currentPageNumber,
+      errors: [],
+      nextPageAvailable: false,
+    };
+  }
+
+  async scrape() {
+    await this.page.goto(this.nextPageUrl, {
       waitUntil: "domcontentloaded",
     });
 
-    const pageHtml: HTMLElement = parse(await page.content());
+    const pageHtml: HTMLElement = parse(await this.page.content());
 
     const elements: HTMLElement[] = pageHtml.querySelectorAll(
       "div[class^=ProductCardstyles__Wrapper-]"
     );
 
-    report.elementsFound = elements.length;
+    this.currentReport.elementsFound = elements.length;
 
-    if (storePage.itemsPerPage !== report.elementsFound)
-      report.errors.push({
-        expected: `Elements count ${storePage.itemsPerPage}`,
-        received: `Elements count ${report.elementsFound}`,
-        severity: report.elementsFound == 0 ? "high" : "low",
+    if (this.storePage.itemsPerPage !== this.currentReport.elementsFound)
+      this.onError({
+        expected: `Elements count ${this.storePage.itemsPerPage}`,
+        received: `Elements count ${this.currentReport.elementsFound}`,
+        severity: this.currentReport.elementsFound == 0 ? "high" : "low",
+        operation:
+          "Checking if count of parsed elements matches expected count of elements.",
       });
 
-    if (report.elementsFound > 0) {
+    if (this.currentReport.elementsFound > 0) {
       // parse elements here
       // parse elements here
     }
 
-    report.nextPageAvailable = checkNextPageAvailable(
+    this.currentReport.nextPageAvailable = this.checkNextPageAvailable(
       pageHtml,
-      currentPageNumber
+      this.currentPageNumber
     );
 
-    if (report.nextPageAvailable) {
-      currentPageNumber += 1;
-      nextPageUrl = parseNextPageUrl(nextPageUrl, currentPageNumber);
+    if (!this.currentReport.nextPageAvailable) {
+      this.onReportFinish();
+      this.resetReport();
+      this.nextPageAvailable = false;
     }
 
-    if (!report.nextPageAvailable) {
-      nextPageAvailable = false;
+    if (this.currentReport.nextPageAvailable) {
+      this.currentPageNumber += 1;
+      this.nextPageUrl = this.parseNextPageUrl(
+        this.nextPageUrl,
+        this.currentPageNumber
+      );
+      this.onReportFinish();
+      this.resetReport();
+      await this.scrape();
     }
+  }
 
-    report.finishedAt = moment().toDate();
-    reports.push(report);
+  checkNextPageAvailable(
+    pageHtml: HTMLElement,
+    currentPageNumber: number
+  ): boolean {
+    const resultsCount: string =
+      pageHtml
+        .querySelector("span[class^=styles__ResultsCount]")
+        ?.getAttribute("data-search-results") || "63";
+    const count: number = parseInt(resultsCount);
+    const totalPages = Math.ceil(count / 63);
+    if (currentPageNumber < totalPages) {
+      return true;
+    }
+    return false;
+  }
+
+  parseNextPageUrl(pageUrl: string, currentPageNumber: number): string {
+    const pageUrlSplit: string[] = pageUrl.split("page:");
+    return `${pageUrlSplit[0]}page:${currentPageNumber}/`;
   }
 }
-
-const checkNextPageAvailable = (
-  pageHtml: HTMLElement,
-  currentPageNumber: number
-): boolean => {
-  const resultsCount: string =
-    pageHtml
-      .querySelector("span[class^=styles__ResultsCount]")
-      ?.getAttribute("data-search-results") || "63";
-  const count: number = parseInt(resultsCount);
-  const totalPages = Math.ceil(count / 63);
-  if (currentPageNumber < totalPages) {
-    return true;
-  }
-  return false;
-};
-
-const parseNextPageUrl = (
-  pageUrl: string,
-  currentPageNumber: number
-): string => {
-  const pageUrlSplit: string[] = pageUrl.split("page:");
-  return `${pageUrlSplit[0]}page:${currentPageNumber}/`;
-};
