0d52c717448dddb21fd040957a647148f8916563
Martin <mselvenis@gmail.com>
2022-01-16 19:51:27 +0000

    cron jobs

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,5 +1,4 @@
 import { BrowserServiceComponent } from "./mediator/browserService.component";
-import { prisma } from "./prisma";
 import puppeteer from "puppeteer-extra";
 import StealthPlugin from "puppeteer-extra-plugin-stealth";
 import { ArgosParserServiceComponent } from "./mediator/parserService.component";
@@ -7,6 +6,7 @@ import { Spider } from "./mediator/spider";
 import { PageService } from "./service/page.service";
 puppeteer.use(StealthPlugin());
 puppeteer.use(require("puppeteer-extra-plugin-anonymize-ua")());
+var cron = require("node-cron");
 
 const pageService: PageService = new PageService();
 const browserService: BrowserServiceComponent = new BrowserServiceComponent();
@@ -15,14 +15,20 @@ const argosParser: ArgosParserServiceComponent =
 const spider: Spider = new Spider(pageService, browserService, [argosParser]);
 
 // A `main` function so that you can use async/await
-async function main() {
-  await spider.run();
-}
+// async function main() {
+//   await spider.run();
+// }
 
-main()
-  .catch((e) => {
-    throw e;
-  })
-  .finally(async () => {
-    await prisma.$disconnect();
-  });
+cron.schedule("* * * * *", async () => {
+  console.log("running a task every minute");
+  // prisma.$connect();
+  spider.run();
+});
+
+// main()
+//   .catch((e) => {
+//     throw e;
+//   })
+//   .finally(async () => {
+//     await prisma.$disconnect();
+//   });
