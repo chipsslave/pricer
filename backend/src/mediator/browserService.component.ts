c465a55dd9d23d53a7de29a1342a49763770fa95
Martin <mselvenis@gmail.com>
2022-01-20 23:17:40 +0000

    fix: sessions issue

--- backend/src/mediator/browserService.component.ts
+++ backend/src/mediator/browserService.component.ts
@@ -9,7 +9,7 @@ puppeteer.use(require("puppeteer-extra-plugin-stealth")());
 puppeteer.use(require("puppeteer-extra-plugin-anonymize-ua")());
 
 export class BrowserServiceComponent {
-  private browser: Browser;
+  private browser: Browser | null;
 
   async launch(
     launchArgs: BrowserLaunchArgumentOptions = {
@@ -30,13 +30,18 @@ export class BrowserServiceComponent {
         })
       | undefined = { waitUntil: ["domcontentloaded", "networkidle2"] }
   ): Promise<void> {
+    if (!this.browser)
+      throw new Error("Trying to get pages from null browser!");
     const pages: Page[] = await this.browser.pages();
     if (pages.length === 0)
       throw new Error("Browser is not launched. RUN launch() first.");
+
     await pages[0].goto(url, options);
   }
 
   async getPageHtmlContent(): Promise<string> {
+    if (!this.browser)
+      throw new Error("Trying to get pages from null browser!");
     const pages: Page[] = await this.browser.pages();
     if (pages.length === 0)
       throw new Error("Browser is not launched. RUN launch() first.");
@@ -44,6 +49,8 @@ export class BrowserServiceComponent {
   }
 
   async getPage(): Promise<Page> {
+    if (!this.browser)
+      throw new Error("Trying to get pages from null browser!");
     const pages: Page[] = await this.browser.pages();
     if (pages.length === 0)
       throw new Error("Browser is not launched. RUN launch() first.");
@@ -52,9 +59,11 @@ export class BrowserServiceComponent {
 
   async close(): Promise<void> {
     if (this.browser) await this.browser.close();
+    this.browser = null;
   }
 
   getBrowser(): Browser {
+    if (!this.browser) throw new Error("Trying to return null browser!");
     return this.browser;
   }
 }
