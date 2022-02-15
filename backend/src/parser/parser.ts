5ddd5087bdde91f174f2e6596a5e26ea6d860645
Martin <mselvenis@gmail.com>
2022-02-15 22:21:26 +0000

    parser rewrite

--- backend/src/parser/parser.ts
+++ backend/src/parser/parser.ts
@@ -1,22 +1,14 @@
-import { JobError } from "../service/jobService.component";
-import { Page } from "@prisma/client";
 import { RequestInit } from "node-fetch";
+import { JobErrorSeverity } from "@prisma/client";
 
 export type ParsedItem = {
-  title: string | null;
-  upc: string | null;
-  price: number | null;
-  url: string | null;
+  title?: string;
+  upc?: string;
+  price?: number;
+  url?: string;
   image?: string;
   brand?: string;
   model?: string;
-};
-
-export type ParsedElementItem = {
-  elementHash: string;
-  elementIndex: number;
-  item: ParsedItem;
-  success: boolean;
 };
 
 export type NextPageResult = {
@@ -24,35 +16,75 @@ export type NextPageResult = {
   pageNumber: number;
 } | null;
 
+export type JobError = {
+  expected: string;
+  result: string;
+  severity: JobErrorSeverity;
+  operation: string;
+  elementIndex: number;
+};
+
 export type ParserResult = {
-  page: Page;
-  pageUrl: string;
-  pageNumber: number;
-  parsedItems: ParsedElementItem[];
+  parsedItems: ParsedItem[];
   elementsCount: number;
   nextPage: NextPageResult;
   parserErrors: JobError[];
+  parsedItemsCount: number;
 };
 
-export interface Parser<T> {
+export type ParserConfig = {
+  currentPageNumber: number;
+  itemElementsCountExpected: number;
+  currentUrl: string;
+};
+
+export interface Parser {
   parse(): ParserResult;
-  buildBody(): RequestInit;
 }
 
-export abstract class AbstractParser<T, IS> implements Parser<T> {
+export abstract class AbstractParser<SC, C, IE> implements Parser {
+  private config: ParserConfig;
   private upcCode: string;
-  private content: T;
+  content: C;
+  result: ParserResult;
 
   constructor(upcCode: string) {
     this.upcCode = upcCode;
   }
 
-  abstract buildBody(): RequestInit;
+  abstract setContent(content: SC): void;
 
-  abstract parseItemElements(): IS;
+  abstract buildBody(): RequestInit | null;
 
-  setContent(content: T): void {
-    this.content = content;
+  abstract parseItemElements(): IE[];
+
+  abstract nextPageExists(): boolean;
+
+  getContent(): C {
+    return this.content;
+  }
+
+  abstract parseTitle(itemElement: IE, index: number): string | undefined;
+  abstract parseUpc(itemElement: IE, index: number): string | undefined;
+  abstract parsePrice(itemElement: IE, index: number): number | undefined;
+  abstract parseUrl(itemElement: IE, index: number): string | undefined;
+  abstract parseImage(itemElement: IE, index: number): string | undefined;
+  abstract parseBrand(itemElement: IE, index: number): string | undefined;
+  abstract parseModel(itemElement: IE, index: number): string | undefined;
+
+  abstract parseNextPageUrl(): string;
+  abstract parseNextPageNumber(): number;
+
+  parseItem(itemElement: IE, index: number): ParsedItem {
+    return {
+      title: this.parseTitle(itemElement, index)?.trim(),
+      upc: this.parseUpc(itemElement, index)?.trim(),
+      price: this.parsePrice(itemElement, index),
+      url: this.parseUrl(itemElement, index)?.trim(),
+      image: this.parseImage(itemElement, index)?.trim(),
+      brand: this.parseBrand(itemElement, index)?.trim(),
+      model: this.parseModel(itemElement, index)?.trim(),
+    };
   }
 
   itemSuccess(item: ParsedItem): boolean {
@@ -64,7 +96,48 @@ export abstract class AbstractParser<T, IS> implements Parser<T> {
     return this.upcCode;
   }
 
-  parse(): ParserResult {}
-}
+  setConfig(config: ParserConfig): void {
+    this.config = config;
+  }
 
-// class CC extends AbstractParser<string> {}
+  getConfig(): ParserConfig {
+    return this.config;
+  }
+
+  parse(): ParserResult {
+    if (!this.config) throw new Error("Config is not set!");
+
+    this.resetResult();
+
+    const parsedElements: IE[] = this.parseItemElements();
+
+    this.result.elementsCount = parsedElements.length;
+
+    for (const [index, element] of parsedElements.entries()) {
+      const parsedItem: ParsedItem = this.parseItem(element, index);
+
+      if (this.itemSuccess(parsedItem))
+        this.result.parsedItems.push(parsedItem);
+    }
+
+    this.result.parsedItemsCount = this.result.parsedItems.length;
+
+    if (this.nextPageExists()) {
+      this.result.nextPage = {
+        pageNumber: this.parseNextPageNumber(),
+        pageUrl: this.parseNextPageUrl(),
+      };
+    }
+    return this.result;
+  }
+
+  resetResult(): void {
+    this.result = {
+      elementsCount: -1,
+      nextPage: null,
+      parsedItems: [],
+      parserErrors: [],
+      parsedItemsCount: 0,
+    };
+  }
+}
