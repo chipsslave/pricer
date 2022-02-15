6e113ba3798424bbc635881b59cfac2762010e1b
Martin <mselvenis@gmail.com>
2022-02-15 18:04:20 +0000

    fetch client abstraction

--- backend/src/browser/fetchClient.ts
+++ backend/src/browser/fetchClient.ts
@@ -1,6 +1,15 @@
 const fetch = require("node-fetch");
 import { Response } from "node-fetch";
 
+async function fetchContent(url: string, body?: any): Promise<Response> {
+  const response: Response = body ? await fetch(url, body) : await fetch(url);
+  if (!response.ok) {
+    console.log({ response });
+    throw new Error("Response is not OK.");
+  }
+  return response;
+}
+
 export class FetchClient {
   private async fetch(url: string, body?: any): Promise<Response> {
     const response: Response = body ? await fetch(url, body) : await fetch(url);
@@ -19,5 +28,21 @@ export class FetchClient {
   async fetchJson(url: string, body?: any): Promise<unknown> {
     const response: Response = await this.fetch(url, body);
     return await response.json();
+  }
+}
+
+export class FetchHtmlClient {
+  async getContent(url: string, body?: any): Promise<string> {
+    return body
+      ? (await fetchContent(url, body)).text()
+      : (await fetchContent(url)).text();
+  }
+}
+
+export class FetchJsonClient<T> {
+  async getContent(url: string, body?: any): Promise<T> {
+    return body
+      ? ((await fetchContent(url, body)).json() as unknown as T)
+      : ((await fetchContent(url)).json() as unknown as T);
   }
 }
