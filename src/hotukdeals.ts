ebb7e5bb2b4d2db8ef54c98c89b341ff6bd09c1e
Martin <mselvenis@gmail.com>
2023-10-18 18:06:33 +0100

    puppeteer extras

--- src/hotukdeals.ts
+++ src/hotukdeals.ts
@@ -1,16 +1,22 @@
-import puppeteer, { Browser } from "puppeteer";
+import { Browser } from "puppeteer";
 import { saveToFile } from "../utils/saveToFile";
 
 export async function run() {
   let browser: Browser | undefined;
   try {
+    const puppeteer = require('puppeteer-extra');
+    const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+    puppeteer.use(StealthPlugin())
+    const AdblockerPlugin = require('puppeteer-extra-plugin-adblocker')
+    puppeteer.use(AdblockerPlugin({ blockTrackers: true }))
+
     browser = await puppeteer.launch({ headless: "new", args: ['--no-sandbox', '--disable-setuid-sandbox'] });
-    const page = await browser.newPage();
+    const page = await browser?.newPage();
 
     const url = "https://www.hotukdeals.com/new";
-    await page.goto(url);
+    await page?.goto(url);
 
-    const content = await page.content();
+    const content = await page?.content();
     const timestamp = Date.now();
     const uniqueId = Math.random().toString(36).substr(2, 9); // generating a random unique id
 
@@ -22,7 +28,7 @@ export async function run() {
 
     const filename = `${timestamp}_${uniqueId}_${cleanedUrl}.html`;
 
-    saveToFile(content, filename);
+    saveToFile(content ? content : "", filename);
 
     console.log(`Page source saved to ${filename}`);
   } finally {
